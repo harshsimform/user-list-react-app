da64edc1e6cb5e23503e678e96d41de885ff6e79
harshsimform <harsh.v@simformsolutions.com>
2023-05-05 17:57:58 +0530

    added extra properties in json file and improved UI

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import Active from "../assets/lock.svg";
 import Lock from "../assets/trash.svg";
 import userRecord from "./userRecord.json";
-import ProgressBar from "./ProgressBar";
+import HoveredUserDetails from "./HoveredUserDetails";
 
-interface User {
+export interface User {
   id: string;
   first_name: string;
   last_name: string;
@@ -13,6 +13,9 @@ interface User {
   active: boolean;
   owner: boolean;
   role: string;
+  plan: string;
+  clicks_reviewed: string;
+  monthly_click: string;
 }
 
 const UserList = (): JSX.Element => {
@@ -98,64 +101,7 @@ const UserList = (): JSX.Element => {
           </table>
         </div>
         <div className="w-3/12 ">
-          {hoveredUser && (
-            <div className="ml-10 max-w-[350px] h-[500px] mt-20 hidden xl:block rounded-[50px] shadow-lg">
-              <div className="mt-16 flex flex-col items-center">
-                <div className="text-xl font-bold text-navy-700 dark:text-white">
-                  <img
-                    className="mt-5 object-cover w-35 h-35 rounded-full"
-                    src={hoveredUser.avatar}
-                    alt={hoveredUser.first_name}
-                  />
-                </div>
-                <div className="flex">
-                  <h1 className="mt-5 font-bold text-lg">
-                    {hoveredUser.first_name} {hoveredUser.last_name}
-                  </h1>
-                  {hoveredUser.active ? (
-                    <span className="relative top-[26px] left-1 h-[7px] w-[7px] rounded-full bg-green-400"></span>
-                  ) : (
-                    <span className="relative top-[26px] left-1 h-[7px] w-[7px] rounded-full bg-orange-400"></span>
-                  )}
-                </div>
-                <div className="flex">
-                  <h1 className="mt-2 text-base font-normal text-gray-600">
-                    {hoveredUser.email}
-                  </h1>
-                </div>
-                <div className="mt-2 flex-col items-center">
-                  <p className="text-xl font-bold text-navy-700">
-                    Your Plan: Standard
-                  </p>
-                  <button className="mt-3 bg-custom-orange text-white font-bold py-2 px-4 w-full rounded-md">
-                    Active User
-                  </button>
-                </div>
-                <div className="mt-5 w-[250px] flex-col justify-start">
-                  <h4 className="text-xl font-bold text-navy-700">Plan Uses</h4>
-                  <ProgressBar progressPercentage={80} />
-                </div>
-                <div className="mt-4 flex justify-center w-[250px] divide-gray-200 divide-x-[3px]">
-                  <div className="text-left flex-1">
-                    <h1 className="mt-2 text-[30px] font-bold text-navy-700">
-                      2,450
-                    </h1>
-                    <p className="text-base text-[15px] text-gray-600">
-                      Clicks Reviewed
-                    </p>
-                  </div>
-                  <div className="text-left flex-1">
-                    <h1 className="mt-2 ml-4 text-[30px] font-bold text-navy-700">
-                      5000
-                    </h1>
-                    <p className="ml-4 text-base text-[15px] text-gray-600">
-                      Monthly Clicks
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
+          {hoveredUser && <HoveredUserDetails hoveredUser={hoveredUser} />}
         </div>
       </div>
     </>
